d075ecd8ef0cd51147c5d28f6655e08a50a97b3e
iTSUKi-0425 <75007909+iTSUKi-0425@users.noreply.github.com>
2020-12-14 12:39:05 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:calendar).permit(:date, :plan)
   end
 
   def get_week
@@ -37,10 +37,10 @@ class CalendarsController < ApplicationController
 
       # wday_num = # wdayメソッドを用いて取得した数値
       # if #「wday_numが7以上の場合」という条件式
-        # wday_num = wday_num -7
+      # wday_num = wday_num -7
       # end
 
-      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans}
+      days = { month: (@todays_date + x).month, date: @todays_date.day + x, plans: today_plans}
       @week_days.push(days)
     end
 
